92504b696d383ba759799fde5dcc04f6a7284727
Andleeb Hayat <74811371+andleebhayath@users.noreply.github.com>
2024-10-09 00:55:24 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
-st.title('👍 My First Streamlit app')
+st.title('👍 Machine Learning  app')
 
-st.write('Hello world!')
+st.info('This App builds a machine learning model')
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+print(df)
+
