34da79fba800fc114c722a29f47571bda462a8e6
Andleeb Hayat <74811371+andleebhayath@users.noreply.github.com>
2024-10-09 01:21:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ with st.expander('Data'):
   y_raw= df.species
   y_raw
   with st.expander('Data visualization'):
-  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+    st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
   
   
 
