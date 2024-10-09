11597453aad2ea41f307252f8c8977bdf890e336
Andleeb Hayat <74811371+andleebhayath@users.noreply.github.com>
2024-10-09 02:20:16 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,12 @@ with st.sidebar:
           'body_mass_g'      : Body_mass_(g),
           'Sex':Gender}
   input_df = pd.DataFrame(data, index=[0])
+  input_penguins = pd.concat([input_df, x_raw], axis=0)
+with st.expander('Input features'):
+  st.write('**Input penguin**')
   input_df
+  st.write('**Combined penguins data**')
+  input_penguins
  
           
        
